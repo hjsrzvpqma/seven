5c4bfdb9ba6bc7204950234b55e6c30d7b42287f
hjsrzvpqma <makan.j100100@gmail.com>
2016-05-07 21:35:05 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -235,7 +235,7 @@ function create_config( )
     },
     sudo_users = {90285047},--Sudo users
     moderation = {data = 'data/moderation.json'},
-    about_text = [[TeleMoon V.4
+    about_text = [[SEVENTG V.1.2
 An advanced administration bot based on TG-CLI written in Lua
 
 
@@ -249,7 +249,7 @@ Amir
 Reza
 
 Our channe
-MoonTeam
+@MoonTeam
 ]],
     help_text_realm = [[
 Realm Commands:
